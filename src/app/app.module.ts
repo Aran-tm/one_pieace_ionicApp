f3fea46c8c87b5f4e894b5ae70d3b2148a090a82
Aran-tm <armandortmontes@gmail.com>
2024-05-25 14:48:37 -0500

    configurando modulos y lenguaje como servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { createTranslationLoader } from '@angular-devkit/build-angular/src/utils/load-translations';
+
+export function tranlateFunction(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n', '.json')
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot({mode: 'md'}),    // estilo de design de android
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslationLoader),
+        deps: [HttpClient]
+      }
+    }),
+    HttpClientModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
