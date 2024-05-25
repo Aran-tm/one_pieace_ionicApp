6533d2c8954e12db20163d413e4e0e901c1dd796
Aran-tm <armandortmontes@gmail.com>
2024-05-25 12:54:36 -0500

    instalando biblioteca de animacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  
 ];
 
 @NgModule({
