a84a5c969d2fb49e4ecdb558444e2d0581574930
Aran-tm <armandortmontes@gmail.com>
2024-05-27 12:56:08 -0500

    realizando importaciones importantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json')
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/language.service.ts
@@ -0,0 +1,44 @@
+import { inject, Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+
+// indica que LanguageService es un servicio
+// que debe ser disponible globalmente en toda la aplicación
+@Injectable({
+  providedIn: 'root'    // disponible globalmente en toda la aplicacion
+})
+export class LanguageService {
+
+  // servicio para la traduccion de paginas
+  translate2 = inject(TranslateService);
+
+  constructor(
+
+    // alternativa a la injeccion del servicio de traduccion
+    private translate: TranslateService
+  ) { }
+
+  // establecer un idioma inicial por defecto
+  setInitialLanguage() {
+
+    // obtengo el idioma del navegador en forma de cadena
+    let language = this.translate.getBrowserLang() as string;
+
+    // guardo en el local storage del navegador, el idioma del navegador
+    let savlanguage = localStorage.getItem('language');
+
+    // establezco por default el idioma
+    this.translate.setDefaultLang(language);
+
+    if (savlanguage)
+      this.setLanguage(savlanguage);
+    else
+      this.setLanguage(language);
+  }
+
+  // permite cambiar el idioma de la aplicacion
+  setLanguage(lang: string) {
+    this.translate.use(lang);
+    localStorage.setItem('language', lang);
+  }
+}
