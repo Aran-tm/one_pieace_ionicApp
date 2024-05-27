8f8aa68195833d5ea7c58ba7c38bcd449476ce23
Aran-tm <armandortmontes@gmail.com>
2024-05-27 11:13:36 -0500

    injecting trouter service into intro page

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPage implements OnInit {
 
-  constructor() { }
+  // se inyecta la dependencia Router
+  router = inject(Router);
+
+  constructor(
+    // private router: Router  OTRA VIA para injectar el servicio
+  ) { }
+
+  // ciclo de vida de angular
+  ionViewWillEnter(){
+
+    // ejecuta codigo cuando el usuario entra a la pagina
+    setTimeout(() => {
+      this.router.navigateByUrl('/home')
+    }, 2800)
+  }
 
   ngOnInit() {
+    console.log(`hey`);
   }
 
 }
